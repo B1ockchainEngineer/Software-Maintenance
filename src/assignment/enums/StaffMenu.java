cc47dba0a8caf1933b747a9376693fabae7b9698
Joseph Lai <josephlai5566@gmail.com>
2025-12-05 21:09:42 +0800

    .

--- src/assignment/enums/StaffMenu.java
+++ src/assignment/enums/StaffMenu.java
@@ -3,16 +3,9 @@ package assignment.enums;
 public enum StaffMenu {
     BACK_TO_MAIN(0, "BACK TO MAIN MENU"),
     ADD_STAFF(1, "ADD NEW STAFF"),
-<<<<<<< HEAD
-    UPDATE_STAFF(2, "UPDATE STAFF INFORMATION"),
-    DELETE_STAFF(3, "DELETE STAFF"),
-    SEARCH_STAFF(4, "SEARCH STAFF"),
-    VIEW_STAFF_LIST(5, "VIEW REGISTERED STAFF LIST");
-=======
     DELETE_STAFF(2, "DELETE STAFF"),
     SEARCH_STAFF(3, "SEARCH STAFF"),
     VIEW_STAFF_LIST(4, "VIEW REGISTERED STAFF LIST");
->>>>>>> 93b2678c1e7167896ea46aa3955f0958e6d6ea66
 
     private final int option;
     private final String description;
